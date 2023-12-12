2769ca9e61cfc0c639f8842985fe4aafd43ee545
giladk123 <giladk@gmail.com>
2023-12-12 21:37:28 +0200

    added module conditons

--- main.tf
+++ main.tf
@@ -7,5 +7,17 @@ module "vnet" {
   address_space = local.vnet_settings.address_space
   subnet_objects = local.subnet_objects
   nsg_objects = local.nsg_objects
+  count   = var.use_module_dev ? 1 : 0
 }
-#123
+
+module "vnet" {
+  source  = "app.terraform.io/hcta-azure-prod/vnet/azurerm"
+  version = "2.0.0"
+  resource_group = local.vnet_settings.resource_group
+  location = local.vnet_settings.location
+  vnet_name = local.vnet_settings.vnet_name
+  address_space = local.vnet_settings.address_space
+  subnet_objects = local.subnet_objects
+  nsg_objects = local.nsg_objects
+  count   = var.use_module_dev ? 0 : 1
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
    description = "The target environment (dev, uat, prod)"
    type        = string
 }
+
+variable "use_module_dev" {
+  description = "Set to true to use module dev, false for module prod"
+  type        = bool
+}
