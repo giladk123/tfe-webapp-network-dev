5037fa008a56cf56b4675e49531e989211bf49b3
giladk123 <giladk@gmail.com>
2023-12-12 22:25:02 +0200

    remove module and variable

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "vnet-dev" {
+module "vnet" {
   source  = "app.terraform.io/hcta-azure-dev/vnet/azurerm"
   version = "2.0.0"
   resource_group = local.vnet_settings.resource_group
@@ -7,17 +7,4 @@ module "vnet-dev" {
   address_space = local.vnet_settings.address_space
   subnet_objects = local.subnet_objects
   nsg_objects = local.nsg_objects
-  count   = var.use_module_dev ? 1 : 0
-}
-
-module "vnet-prod" {
-  source  = "app.terraform.io/hcta-azure-prod/vnet/azurerm"
-  version = "2.0.0"
-  resource_group = local.vnet_settings.resource_group
-  location = local.vnet_settings.location
-  vnet_name = local.vnet_settings.vnet_name
-  address_space = local.vnet_settings.address_space
-  subnet_objects = local.subnet_objects
-  nsg_objects = local.nsg_objects
-  count   = var.use_module_dev ? 0 : 1
 }
