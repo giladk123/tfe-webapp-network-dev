0ff64c96c4b9492e2e247febebdf69f197419518
Gilad Kalev <giladk@gmail.com>
2025-06-26 09:09:40 +0300

    new storage account module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "rg_id" {
-  value = module.resource_group.resource_groups
+  value = module.resource-group.resource_groups["testing"].id
+}
+
+output "rg_name" {
+  value = module.resource-group.resource_groups["testing"].name
+}
+
+output "rg_location" {
+  value = module.resource-group.resource_groups["testing"].location
+}
+
+# Optional: If you want to output all resource groups
+output "all_resource_groups" {
+  value = module.resource-group.resource_groups
 }
