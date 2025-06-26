ad3d5b4a47507a372296d10fc044a1168eca4b1d
Gilad Kalev <giladk@gmail.com>
2025-06-26 15:47:24 +0300

    update resource group readme

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
-  resource_group = jsondecode(file("ccoe/rg.json"))
-  vnet_settings  = jsondecode(file("network/vnet.json"))
+  resource_group   = jsondecode(file("ccoe/rg.json"))
+  vnet_settings    = jsondecode(file("network/vnet.json"))
+  storage_accounts = jsondecode(file("ccoe/storage-accounts.json"))
 }
