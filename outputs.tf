52f4bae8a1d6061d1084e2d14a116d1b0d81fb4a
Gilad Kalev <giladk@gmail.com>
2025-06-26 08:38:09 +0300

    new storage account module

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "rg_id" {
-  value = module.resource-group.rg_id
+  value = module.resource-group.resource_groups.testing.rg_id
 }
 
 output "rg_name" {
-  value = module.resource-group.rg_name
+  value = module.resource-group.resource_groups.testing.rg_name
 }
 
 output "rg_location" {
-  value = module.resource-group.rg_location
+  value = module.resource-group.resource_groups.testing.rg_location
 }
