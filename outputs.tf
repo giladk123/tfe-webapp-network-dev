754c9ab23b3328c912e48371b818fb9bfed60e95
Gilad Kalev <giladk@gmail.com>
2025-06-26 13:09:46 +0300

    update resource vnet

--- outputs.tf
+++ outputs.tf
@@ -18,53 +18,61 @@ output "all_resource_groups" {
 # VNet outputs
 output "vnet_id" {
   description = "The ID of the virtual network"
-  value       = module.modules_vnet.vnets["testing"].id
+  value       = module.modules_vnet.vnet["testing"].id
 }
 
 output "vnet_name" {
   description = "The name of the virtual network"
-  value       = module.modules_vnet.vnets["testing"].name
+  value       = module.modules_vnet.vnet["testing"].name
 }
 
 output "vnet_location" {
   description = "The location of the virtual network"
-  value       = module.modules_vnet.vnets["testing"].location
+  value       = module.modules_vnet.vnet["testing"].location
 }
 
 output "vnet_address_space" {
   description = "The address space of the virtual network"
-  value       = module.modules_vnet.vnets["testing"].address_space
+  value       = module.modules_vnet.vnet["testing"].address_space
 }
 
 output "vnet_resource_group_name" {
   description = "The resource group name of the virtual network"
-  value       = module.modules_vnet.vnets["testing"].resource_group_name
+  value       = module.modules_vnet.vnet["testing"].resource_group_name
 }
 
 # Subnet outputs
-output "vnet_subnets" {
-  description = "All subnet information for the virtual network"
-  value       = module.modules_vnet.vnets["testing"].subnets
-}
-
 output "subnet_ids" {
   description = "Map of subnet names to their IDs"
   value = {
-    for subnet in module.modules_vnet.vnets["testing"].subnets :
-    subnet.name => subnet.id
+    for k, v in module.modules_vnet.subnet : k => v.id
+    if startswith(k, "testing-")
+  }
+}
+
+output "subnet_names" {
+  description = "Map of subnet keys to their names"
+  value = {
+    for k, v in module.modules_vnet.subnet : k => v.name
+    if startswith(k, "testing-")
   }
 }
 
 output "subnet_address_prefixes" {
   description = "Map of subnet names to their address prefixes"
   value = {
-    for subnet in module.modules_vnet.vnets["testing"].subnets :
-    subnet.name => subnet.address_prefixes[0]
+    for k, v in module.modules_vnet.subnet : k => v.address_prefixes
+    if startswith(k, "testing-")
   }
 }
 
-# All VNets output (if you have multiple vnets in the future)
+# All resources output
 output "all_vnets" {
   description = "All virtual networks managed by this module"
-  value       = module.modules_vnet.vnets
+  value       = module.modules_vnet.vnet
+}
+
+output "all_subnets" {
+  description = "All subnets managed by this module"
+  value       = module.modules_vnet.subnet
 }
