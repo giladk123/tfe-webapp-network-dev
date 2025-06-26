61c40715736a6a46c819f4adc966ed61ccb76bc9
Gilad Kalev <giladk@gmail.com>
2025-06-26 11:07:37 +0300

    update resource vnet

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
   resource_group = jsondecode(file("ccoe/rg.json"))
+  vnet_settings  = jsondecode(file("network/vnet.json"))
 }
--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ module "resource-group" {
   name_convention = local.resource_group.resource_groups.testing.name_convention
 }
 
+module "modules_vnet" {
+  source  = "app.terraform.io/hcta-azure-dev/modules/azurerm//modules/vnet"
+  version = "1.0.51"
+
+  vnets = local.vnet_settings.vnets
+
+  depends_on = [
+    module.resource-group
+  ]
+}
