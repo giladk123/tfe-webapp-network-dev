0a499aee6609a8c36bb68c3af0660a9235f6f8d6
giladk123 <83569702+giladk123@users.noreply.github.com>
2025-06-27 14:12:44 +0300

    added outputs to storage account

--- outputs.tf
+++ outputs.tf
@@ -76,3 +76,74 @@ output "all_subnets" {
   description = "All subnets managed by this module"
   value       = module.modules_vnet.subnet
 }
+
+# Storage Account outputs
+output "storage_account_ids" {
+  description = "Map of storage account keys to their resource IDs"
+  value       = module.modules_storage_account.storage_account_ids
+}
+
+output "storage_account_names" {
+  description = "Map of storage account keys to their actual names"
+  value       = module.modules_storage_account.storage_account_names
+}
+
+output "storage_account_primary_blob_endpoints" {
+  description = "Map of storage account keys to their primary blob endpoints"
+  value       = module.modules_storage_account.primary_blob_endpoints
+}
+
+output "storage_account_primary_connection_strings" {
+  description = "Map of storage account keys to their primary connection strings"
+  value       = module.modules_storage_account.primary_connection_strings
+  sensitive   = true
+}
+
+output "storage_accounts" {
+  description = "Comprehensive map of all created storage accounts with their properties"
+  value       = module.modules_storage_account.storage_accounts
+  sensitive   = true
+}
+
+# Storage containers, file shares, queues, and tables outputs
+output "storage_containers" {
+  description = "Map of all created storage containers"
+  value       = module.modules_storage_account.containers
+}
+
+output "storage_file_shares" {
+  description = "Map of all created file shares"
+  value       = module.modules_storage_account.file_shares
+}
+
+output "storage_queues" {
+  description = "Map of all created storage queues"
+  value       = module.modules_storage_account.queues
+}
+
+output "storage_tables" {
+  description = "Map of all created storage tables"
+  value       = module.modules_storage_account.tables
+}
+
+# Private endpoint outputs for storage accounts
+output "storage_private_endpoints" {
+  description = "Map of all created private endpoints for storage accounts"
+  value       = module.modules_storage_account.private_endpoints
+}
+
+# Specific storage account outputs (for the storageaccount001)
+output "storageaccount001_id" {
+  description = "The ID of storageaccount001"
+  value       = try(module.modules_storage_account.storage_account_ids["storageaccount001"], null)
+}
+
+output "storageaccount001_name" {
+  description = "The name of storageaccount001"
+  value       = try(module.modules_storage_account.storage_account_names["storageaccount001"], null)
+}
+
+output "storageaccount001_primary_blob_endpoint" {
+  description = "The primary blob endpoint of storageaccount001"
+  value       = try(module.modules_storage_account.primary_blob_endpoints["storageaccount001"], null)
+}
